32f26775fead51a3536048c32f9b4e7365a7d4c5
mareevea <164815627+mareevea@users.noreply.github.com>
2024-10-16 09:34:17 +0300

    Add files via upload

---
+++ Python_Week4_1.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 1
+x = x+2
+print(x)
+
+
+# In[2]:
+
+
+a = 'Eve' + 'Marealle'
+print(a)
+
+
+# In[3]:
+
+
+xx = 2
+
+xx = 2* xx
+print (xx)
+
+
+# In[4]:
+
+
+#ONLY RETURNS THE REMAINDER
+y=5
+r = 7
+z = y%r
+print (z)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+type (1)
+
+
+# In[6]:
+
+
+E = 1
+type (E)
+
+
+# In[7]:
+
+
+print (float(99)+200)
+
+
+# In[8]:
+
+
+i =42
+f = float (i)
+print (f)
+
+
+# In[9]:
+
+
+print (9/2)
+
+
+# In[10]:
+
+
+print (10/2)
+
+
+# In[11]:
+
+
+print (10.0/2.0)
+
+
+# In[12]:
+
+
+#input inapause afu inaacha ujibu
+nam = input("who are you?")
+print ("welcome", nam) #the space here will result in welcome Chuck not welcomechuck AFTER WRITING NAME TYPE ENTER
+
+
+# In[13]:
+
+
+#changing a string to input to be used for calculations
+string = "123"
+type (string)
+integer = int (string)
+print (integer)
+print (integer+1)
+
+
+# In[14]:
+
+
+#convert elevators floors
+inp = input ("Europefloors")
+US = int (inp) +1
+print ("USFLOOR", US)
+
+
+# In[15]:
+
+
+#ASSIGNMENT
+print("123" + "abc")
+
+
+# In[16]:
+
+
+#BAD VARIABLENAME variable name can start with underscore or letter but not a number
+23SPAM = 3
+print (23SPAM)
+
+
+# In[17]:
+
+
+print (42 % 10)
+
+
+# In[18]:
+
+
+print  (1 + 2 * 3 - 8 / 4)
+
+
+# In[19]:
+
+
+print(int(98.6))
+
+
+# In[20]:
+
+
+name = input ("who are you")
+print ("Good to see you", name)
+
+
+# In[21]:
+
+
+#Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25). You should use input to read a string and float() to convert the string to a number. Do not worry about error checking or bad user data.
+  
+# This first line is provided for you
+
+  
+hrs = input("Enter Hours:")
+rate = input("Enter rate:")
+Pay_is = float (hrs) * float (rate)
+print ("Pay:", Pay_is)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
